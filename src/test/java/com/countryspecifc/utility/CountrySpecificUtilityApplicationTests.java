4baf0be1ab652563c0a353c4db3d1ea791526d01
PrasannaJ333 <prasanna.j@acheron-tech.com>
2023-08-01 16:44:06 +0530

    first commit

---
+++ src/test/java/com/countryspecifc/utility/CountrySpecificUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.countryspecifc.utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrySpecificUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
